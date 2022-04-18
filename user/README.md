5358a508144d938ebd0c05dfd432ce629adb54cb
Ming <mzhao22@buffalo.edu>
2022-04-17 22:05:51 -0400

    #27 The maximum number of task is 10.

--- user/README.md
+++
@@ -1,2 +0,0 @@
-# cse442-spring2022-team-niubility
-cse442-spring2022-team-niubility created by GitHub Classroom
--- user/accounts/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import url, include
-from .views import *
-
-urlpatterns = [
-    url(r"^login$", user_login, name="login"),
-    url(r"^logout$", user_logout, name="logout"),
-    url(r"^register$", do_register, name="register"),
-]
